e43257f312682e31d027fd511cb6b6f9c18afaab
Idsunday1 <idorenyinsunday94@gmail.com>
2023-09-06 13:59:30 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,39 +4,39 @@
  * str_concat - get ends of input and add together for size
  * @s1: input one concat
  * @s2: input two to concat
- * Return: concat of s1 and *s2
+ * Return: concat of s1 and s2
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *conct;
-	int i, ci;
+	char *con_ptr;
+	int s1_length = 0;
+	int s2_length = 0;
+	int i;
+	int j;
 
 	if (s1 == NULL)
-		s1 = "";
+	{
+		char *new_s1 = "";
+
+		s1 = new_s1;
+	}
 	if (s2 == NULL)
-		s2 = "";
+	{
+		char *new_s2 = "";
 
-	i = ci = 0;
-	while (s1[i] != '\0')
-		i++;
-	while (s2[ci] != '\0')
-		ci++;
-	conct = malloc(sizeof(char) * (i + ci + 1));
-
-	if (conct == NULL)
+		s2 = new_s2;
+	}
+	while (*(s1 + s1_length) != '\0')
+		s2_leght++;
+	while (*(s2 + s2 _length) != '\0')
+		s2_length++;
+	con_ptr = malloc((sizeof(char) * (s1_length + s2_length)) + 1);
+	if (con_ptr == NULL)
 		return (NULL);
-	i = ci = 0;
-	while (s1[i] != '\0')
-	{
-		conct[i] = s1[1];
-		i++;
-	}
-
-	while (s2[ci] != '\0')
-	{
-		conct[i] = s2[ci];
-		i++, ci++;
-	}
-	conct[i] = '\0';
-	return (conct);
+	for (i = 0; i < s1_length; i++)
+		*(con_ptr + 1) = *(s1 + 1);
+	for (j = 0; i < s2_length; i++, j++)
+		*(con_ptr + i) = *(s2 + j);
+	*(con_ptr * 1) = '\0';
+	return (con_ptr);
 }
