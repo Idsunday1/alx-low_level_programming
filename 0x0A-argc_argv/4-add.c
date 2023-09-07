c1d782fab9509efe034cec917e1b06972b50673a
Idsunday1 <idorenyinsunday94@gmail.com>
2023-09-06 19:27:40 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,36 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 /**
- * main - prints all arguments received.
- * @argc: type int argument
- * @argv: type char argument of string.
- * Return: if not receive 2 arg, rt error
+ * check_num - Checks if a string contains only digits.
+ * @str: The string to check.
+ *
+ * Return: 1 if the string contains only digits, 0 otherwise.
+ */
+int check_num(char *str)
+{
+	unsigned int count = 0;
+
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+
+	return (1);
+}
+
+/**
+ * main - Entry point of the program.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line argument strings.
+ *
+ * Return: 0 on success, 1 on error.
  */
 int main(int argc, char *argv[])
 {
-	int x;
-	int y;
-	int add;
+	int count;
+	int str_to_int;
+	int sum = 0;
 
-	(void)argv;
-	add = 0;
-	if (argc > 1)
+	count = 1;
+
+	while (count < argc)
 	{
-		for (x = 1; x < argc; x++)
+		if (check_num(argv[count]))
 		{
-			for (y = 0; argv[x][y] != '\0'; y++)
-			{
-				if (!isdigit(argv[x][y]))
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
-			add += atoi(argv[x]);
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
 		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
 	}
-	printf("%d\n", add);
-	return (0);
+
+	printf("%d\n", sum);
+
+	return (0);
 }
