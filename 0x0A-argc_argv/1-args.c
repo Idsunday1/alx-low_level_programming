4c523bec9eaa381ecfd03ebc6932cfdee566d4f7
Idsunday1 <idorenyinsunday94@gmail.com>
2023-09-06 18:12:52 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,17 +2,17 @@
 #include "main.h"
 
 /**
- * main - print the number of arguments passed to the program
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
+ * main - prints the number of arguments passed into it.
+ * @argc: the int argument
+ * @argv: the type char argument of strings
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	(void) argv; / Ignore argv /
-		printf("%d\n", argc - 1);
+	(void)argv;
 
-	return (0);
+	printf("%d\n", argc - 1);
+
+	return 0;
 }
