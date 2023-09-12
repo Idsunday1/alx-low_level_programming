c5cfa4b645c4d3a6e9bfabeb2240128f593109fc
Idsunday1 <idorenyinsunday94@gmail.com>
2023-09-11 17:46:25 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/0x0E-structures_typedef/2-print_dog.c
@@ -9,11 +9,19 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
+	{
 		return;
+	}
 
-	if (->name == NULL)
-		d->name = "(nill)";
+	if (d->name == NULL)
+	{
+		d->name = "(nil)";
+	}
+
 	if (d->owner == NULL)
-		d->owner = "(nill)";
-	printf("name: %s\nAge: %f\nowner: %s\n", d->name, d->age, d->owmner);
+	{
+		d->owner = "(nil)";
+	}
+
+	printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }
