06781a8ee67dea57a56ef818a50870d7491f2102
Idsunday1 <idorenyinsunday94@gmail.com>
2023-10-30 18:30:21 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -45,7 +45,6 @@ char **strtow(char *str)
 	matrix = (char **)malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
 		return (NULL);
-
 	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
